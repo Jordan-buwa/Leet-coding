00e1ae73d9004ca8c59cda8950d438c9ac7dc31e
Your Name <your.email@example.com>
2025-05-22 23:19:52 +0000

    Solution for removeDuplicates

--- leet_code.py
+++ leet_code.py
@@ -28,4 +28,16 @@ class LeetCode:
                 if nums[i] + nums[j] == target:
                     return [i,j]
         return None
+    def removeDuplicates(self, nums: list[int]) -> int:
+        assert len(nums) >= 1 and len(nums)<= 3e+4
+        for i in range(len(nums)):
+            assert abs(nums[i]) <= 100
+        i = 1;  k = 0; n = len(nums)
+        while i < n:
+            if nums[i] != nums[k]:
+                k += 1
+                nums[k] = nums[i] 
+            i += 1
+        k += 1
+        return k
         
